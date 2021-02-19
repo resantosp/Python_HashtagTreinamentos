6cb3d7761b92002ebc6436aec7c199dfb5e7eb5b
Renata Santos <renatados.santosp@gmail.com>
2021-02-19 01:48:06 -0300

    Create Carga_Tributaria.py
    Digamos que você trabalhe em uma empresa de ecommerce:
    
    1. Calcular 10% de imposto sobre o valor dos livros
    2. Alterar o registro dos preços dos livros da empresa
    3. Ajustar o novo valor na tabela
    4. Calcular quando que o imposto vai aumentar de custo para a empresa??
    
    Obs: Seu código deve funcionar mesmo que não haja livros na lista de produtos da empresa

---
+++ Listas_Exercícios/Carga_Tributaria.py
@@ -0,0 +1,27 @@
+'''
+Digamos que você trabalhe em uma empresa de ecommerce
+
+1. Calcular 10% de imposto sobre o valor dos livros
+2. Alterar o registro dos preços dos livros da empresa
+3. Ajustar o novo valor na tabela
+4. Calcular quando que o imposto vai aumentar de custo para a empresa??
+
+Obs: Seu código deve funcionar mesmo que não haja livros na lista de produtos da empresa
+'''
+
+produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
+#cada item da lista dos produtos corresponde a quantidade de vendas no mês e preço, nessa ordem
+produtos_ecommerce = [
+    [10000, 2500],
+    [50000, 40],
+    [7000, 1200],
+    [20000, 1500],
+    [5800, 1300],
+    [7200, 2500],
+    [200, 800],
+    [3300, 700],
+    [1900, 400]
+]
+
+
+
