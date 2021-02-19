9d256553e93b1681ecb6aed9e546c7ed9d805583
Renata Santos <renatados.santosp@gmail.com>
2021-02-19 02:17:59 -0300

    Update Carga_Tributaria.py
    Versão Final.

--- Listas_Exercícios/Carga_Tributaria.py
+++ Listas_Exercícios/Carga_Tributaria.py
@@ -1,12 +1,13 @@
 '''
-Digamos que você trabalhe em uma empresa de ecommerce
+Digamos que você trabalhe em uma empresa de ecommerce. Um novo e-mail chegou avisando uma reforma tributária que
+irá aumentar em 10% o valor do imposto sobre livros.
 
 1. Calcular 10% de imposto sobre o valor dos livros
 2. Alterar o registro dos preços dos livros da empresa
 3. Ajustar o novo valor na tabela
 4. Calcular quanto que o imposto vai aumentar de custo para a empresa??
 
-Obs: Seu código deve funcionar mesmo que não haja livros na lista de produtos da empresa
+Obs: o código deve funcionar mesmo que não haja livros na lista de produtos da empresa
 '''
 
 produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
@@ -26,12 +27,10 @@ produtos_ecommerce = [
 if 'livro' in produtos:
     i = produtos.index('livro')
     total1 = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
-    novo_valor = int(produtos_ecommerce[i][1] + ((10 / 100) * produtos_ecommerce[i][1]))
-    produtos_ecommerce[i][1] = novo_valor
+    produtos_ecommerce[i][1] = int(produtos_ecommerce[i][1] + ((10 / 100) * produtos_ecommerce[i][1]))
     total2 = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
-    custo = total2 - total1
-    print('O novo valor do produto será R${}.'.format(novo_valor))
-    print('O impacto financeiro do aumento de 10% no imposto será de R${:.2f}.'.format(custo))
+    print('O novo valor do produto será R${}.'.format(produtos_ecommerce[i][1]))
+    print('O impacto financeiro do aumento de 10% no imposto será de R${:.2f}.'.format(total2 - total1))
 else:
     print('Não haverá impactos no setor financeiro decorrente da mudança da carga tributária sobre livros.')    
     
