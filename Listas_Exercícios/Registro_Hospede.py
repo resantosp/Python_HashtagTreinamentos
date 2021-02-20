df3abb9ba789927fc1928fcf541c9265330a58ff
Renata Santos <renatados.santosp@gmail.com>
2021-02-20 18:47:22 -0300

    Update Registro_Hospede.py
    Nova versão

--- Listas_Exercícios/Registro_Hospede.py
+++ Listas_Exercícios/Registro_Hospede.py
@@ -6,12 +6,17 @@ print('\n')
 qtd_hospedes = int(input('Quantidade de hóspedes: '))
 quarto = []
 for hospedes in range(qtd_hospedes):
-    print('HÓSPEDE {}'.format(hospedes + 1))
+    print('     HÓSPEDE {}'.format(hospedes + 1))
     nome = input('NOME: ')
     cpf = input('CPF: ')
     hospede = [nome, cpf]
     quarto.append(hospede)
-print(quarto)
+print('\n', '='*8, 'DADOS DO QUARTO', '='*8)
+for i in quarto:
+    indice = quarto.index(i)
+    print('NOME: {}   '.format(quarto[indice][0]),'CPF: {}'.format(quarto[indice][1]))
+
+
 ##problemas extras:
-#1. tentar colocar uma pessoa a mais/menos
+#1. tentar colocar uma pessoa a menos
 #2. digitar cpf errado/inválido
