0625d08b4036ad033448fd05b6eb061caf684b4f
Renata Santos <renatados.santosp@gmail.com>
2021-02-20 19:27:15 -0300

    Update Meta_Vendas.py

--- Listas_Exercícios/Meta_Vendas.py
+++ Listas_Exercícios/Meta_Vendas.py
@@ -7,3 +7,7 @@ vendas = [
     ['Ana', 10300],
     ['Alon', 7870],
 ]
+for vendedor in vendas:
+    indice = vendas.index(vendedor)
+    if vendas[indice][1] < meta:
+        print('{} não bateu a meta mensal. Total de vendas: {}'.format(vendas[indice][0], vendas[indice][1]))
