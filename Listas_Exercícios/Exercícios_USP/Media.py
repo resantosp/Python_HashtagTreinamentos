6fbf36a51b08f6b55719444c3e720efb2ffab003
Renata Santos <renatados.santosp@gmail.com>
2021-03-04 19:03:10 -0300

    Update Media.py

--- Listas_Exercícios/Exercícios_USP/Media.py
+++ Listas_Exercícios/Exercícios_USP/Media.py
@@ -3,4 +3,4 @@ n2 = float(input('Digite a segunda nota: '))
 n3 = float(input('Digite a terceira nota: '))
 n4 = float(input('Digite a quarta nota: '))
 media = (n1 + n2 + n3 + n4) / 4
-print('A média das notas é {}'.format(media))
+print('A média aritmética é', media)
