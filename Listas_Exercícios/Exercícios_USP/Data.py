96d652056f9c58e12d279613fc8fde05dd631f6e
Renata Santos <renatados.santosp@gmail.com>
2021-03-06 02:09:55 -0300

    Update Data.py

--- Listas_Exercícios/Exercícios_USP/Data.py
+++ Listas_Exercícios/Exercícios_USP/Data.py
@@ -2,4 +2,7 @@ n = int(input('Por favor, entre com o número de segundos que deseja converter: 
 dias = n // 86400
 n = n % 86400
 horas = n // 3600
-print(dias, 'e', horas)
+n = n % 3600
+minutos = n // 60
+segundos = n % 60
+print(dias, 'dias,', horas, 'horas,', minutos, 'minutos e', segundos, 'segundos.')
